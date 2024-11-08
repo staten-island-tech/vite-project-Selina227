e3a4a7661f06ce207378f112c6b8ac1a20085581
Selina227 <selinac33@nycstudents.net>
2024-11-07 21:57:58 -0500

    e

--- app/js/main.js
+++ app/js/main.js
@@ -1,10 +1,6 @@
 import { snacks } from "./products.js";
 import "../css/style.css";
 
-const allSnacks = snacks.filter((el) => el.title !== "salad");
-const cheapSnacks = snacks.filter((el) => el.price <= 3.0);
-const expensiveSnacks = snacks.filter((el) => el.price > 3.0);
-
 document.querySelector(".all").addEventListener("click", function (event) {
   event.preventDefault();
   allCards();
@@ -14,6 +10,13 @@ document.querySelector(".cheap").addEventListener("click", function (event) {
   event.preventDefault();
   cheapCards();
 });
+
+document
+  .querySelector(".expensive")
+  .addEventListener("click", function (event) {
+    event.preventDefault();
+    expensiveCards();
+  });
 
 function allCards() {
   const container = document.querySelector(".container");
@@ -32,8 +35,28 @@ function allCards() {
 }
 
 function cheapCards() {
+  const container = document.querySelector(".container");
   container.innerHTML = "";
+  const cheapSnacks = snacks.filter((snack) => snack.price <= 3.0);
+
   cheapSnacks.forEach((snack) => {
+    container.innerHTML += `
+    <div class="card">
+      <h2> ${snack.title} </h2>
+      <h4> ${snack.price} </h4>
+       <h4> ${snack.originCountry} </h4>
+      <img src="${snack.imageUrl}" alt="${snack.altText}">
+      <h5>Flavor: ${snack.flavor} </h5>
+    </div>`;
+  });
+}
+
+function expensiveCards() {
+  const container = document.querySelector(".container");
+  container.innerHTML = "";
+  const expensiveSnacks = snacks.filter((el) => el.price > 3.0);
+
+  expensiveSnacks.forEach((snack) => {
     container.innerHTML += `
     <div class="card">
       <h2> ${snack.title} </h2>
